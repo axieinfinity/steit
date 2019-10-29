a06113b64d02b66663c142e26a232790863448c0
Trung Nguyen <me@trungfinity.com>
2019-10-29 22:03:49 +0700

    Implement (de)serialization for Path

--- vector-state/src/path.rs
+++ vector-state/src/path.rs
@@ -1,4 +1,9 @@
+use std::io;
 use std::rc::{Rc, Weak};
+
+use crate::ser::Serialize;
+use crate::varint::Varint;
+use std::io::Read;
 
 #[derive(Debug)]
 enum Node<T> {
@@ -69,4 +74,101 @@ impl Path {
             node: Rc::new(Node::child(&self.node, tag)),
         }
     }
+
+    pub fn to_values(&self) -> Vec<u16> {
+        self.node.to_values()
+    }
+
+    fn size(node: &Rc<Node<u16>>) -> u32 {
+        match node.as_ref() {
+            Node::Root => 0,
+            Node::Child { parent, value } => {
+                let mut size = Varint::size(value) as u32;
+
+                if let Some(parent) = parent.upgrade() {
+                    size += Self::size(&parent)
+                }
+
+                size
+            }
+        }
+    }
+
+    fn serialize<W: io::Write>(node: &Rc<Node<u16>>, writer: &mut W) -> Result<(), io::Error> {
+        match node.as_ref() {
+            Node::Root => {}
+            Node::Child { parent, value } => {
+                if let Some(parent) = parent.upgrade() {
+                    Self::serialize(&parent, writer)?;
+                }
+
+                Varint::serialize(value, writer)?;
+            }
+        }
+
+        Ok(())
+    }
+}
+
+impl Serialize for Path {
+    fn size(&self) -> u32 {
+        Self::size(&self.node)
+    }
+
+    fn serialize<W: io::Write>(&self, writer: &mut W) -> Result<(), io::Error> {
+        Varint::serialize(&self.size(), writer)?;
+        Self::serialize(&self.node, writer)
+    }
+}
+
+pub fn deserialize<R: io::Read>(reader: &mut R) -> Result<Vec<u16>, io::Error> {
+    let size = Varint::deserialize(reader)?;
+    let reader = &mut iowrap::Eof::new(reader.by_ref().take(size));
+    let mut segments = Vec::new();
+
+    while !reader.eof()? {
+        let segment = Varint::deserialize(reader)?;
+        segments.push(segment);
+    }
+
+    Ok(segments)
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::ser::Serialize;
+
+    use super::deserialize;
+    use super::Path;
+
+    fn assert_back_and_forth(segments: &[u16]) {
+        let mut paths = vec![Path::root()];
+
+        for segment in segments {
+            let path = paths.last().unwrap().child(*segment);
+            paths.push(path);
+        }
+
+        let path = paths.last().unwrap();
+        let mut bytes = Vec::new();
+
+        Serialize::serialize(path, &mut bytes).unwrap();
+
+        assert_eq!(deserialize(&mut &*bytes).unwrap(), segments);
+    }
+
+    macro_rules! t {
+        ($name:ident : $segments:expr) => {
+            #[test]
+            fn $name() {
+                assert_back_and_forth($segments);
+            }
+        };
+    }
+
+    t!(back_and_forth_1 : &[0]);
+    t!(back_and_forth_2 : &[1]);
+    t!(back_and_forth_3 : &[0, 0, 0, 1]);
+    t!(back_and_forth_4 : &[1, 1, 1]);
+    t!(back_and_forth_5 : &[10_000, -1i8 as u16, 137, 1, 2, 3, 4, 0, 0, 42]);
 }
