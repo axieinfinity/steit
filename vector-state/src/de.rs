9eb7cf126aa6a97429247fd708dcb3e0d7dbe134
Trung Nguyen <me@trungfinity.com>
2019-10-29 14:36:15 +0700

    Change Deserialize API to using in-place deserialization

--- vector-state/src/de.rs
+++ vector-state/src/de.rs
@@ -3,11 +3,12 @@ use std::io;
 use crate::varint::Varint;
 
 pub trait Deserialize: Sized {
-    fn deserialize<R: io::Read>(reader: &mut R) -> io::Result<Self>;
+    fn deserialize<R: io::Read>(reader: &mut R, target: &mut Self) -> io::Result<()>;
 }
 
 impl<I: Varint> Deserialize for I {
-    fn deserialize<R: io::Read>(reader: &mut R) -> io::Result<Self> {
-        Varint::deserialize(reader)
+    fn deserialize<R: io::Read>(reader: &mut R, target: &mut Self) -> io::Result<()> {
+        *target = Varint::deserialize(reader)?;
+        Ok(())
     }
 }
