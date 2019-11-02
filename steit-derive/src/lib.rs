4d33017d00dae1cf8bf936ce42e02540743e2804
Trung Nguyen <me@trungfinity.com>
2019-11-03 05:07:00 +0700

    Rename internal module to derive

--- steit-derive/src/lib.rs
+++ steit-derive/src/lib.rs
@@ -4,22 +4,22 @@ extern crate quote;
 
 mod attr;
 mod context;
-mod internal;
+mod derive;
 mod util;
 
-use crate::internal::DeriveKind;
+use crate::derive::DeriveKind;
 
 #[proc_macro_derive(Serialize, attributes(steit))]
 pub fn serialize(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    internal::derive(&DeriveKind::Serialize, input)
+    derive::derive(&DeriveKind::Serialize, input)
 }
 
 #[proc_macro_derive(Deserialize, attributes(steit))]
 pub fn deserialize(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    internal::derive(&DeriveKind::Deserialize, input)
+    derive::derive(&DeriveKind::Deserialize, input)
 }
 
 #[proc_macro_derive(State, attributes(steit))]
 pub fn state(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    internal::derive(&DeriveKind::State, input)
+    derive::derive(&DeriveKind::State, input)
 }
