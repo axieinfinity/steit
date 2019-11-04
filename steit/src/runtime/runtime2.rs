31909ed133547cc5e82d2661e135b1667293a691
Trung Nguyen <me@trungfinity.com>
2019-11-05 06:03:49 +0700

    Upgrade Runtime to store a CachedSize at root

--- steit/src/runtime/runtime2.rs
+++ steit/src/runtime/runtime2.rs
@@ -1,17 +1,22 @@
 use std::{io, rc::Rc};
 
-use crate::{runtime::node::Node, wire_type::WireType, Serialize2};
+use crate::{
+    wire_type::{WireType, WIRE_TYPE_SIZED},
+    Serialize2,
+};
 
-use super::cached_size::CachedSize;
+use super::{cached_size::CachedSize, node::Node};
 
 #[derive(Debug)]
-struct RuntimeInner {
+struct Child {
     tag: u16,
-    /// Cached size of the actual serialized object
+    /// Cached size of the serialized object
+    /// which the current `Runtime` object attaches to
     cached_size: CachedSize,
 }
 
-impl RuntimeInner {
+impl Child {
+    #[inline]
     pub fn new(tag: u16) -> Self {
         Self {
             tag,
@@ -20,11 +25,11 @@ impl RuntimeInner {
     }
 }
 
-impl WireType for RuntimeInner {
+impl WireType for Child {
     const WIRE_TYPE: u8 = <u16 as WireType>::WIRE_TYPE;
 }
 
-impl Serialize2 for RuntimeInner {
+impl Serialize2 for Child {
     #[inline]
     fn size(&self) -> u32 {
         self.tag.size()
@@ -36,9 +41,48 @@ impl Serialize2 for RuntimeInner {
     }
 }
 
+#[derive(Debug)]
+struct Root {
+    /// Cached size of the serialized object
+    /// which the current `Runtime` object attaches to
+    cached_size: CachedSize,
+}
+
+impl Root {
+    #[inline]
+    pub fn new() -> Self {
+        Self {
+            cached_size: CachedSize::unset(),
+        }
+    }
+}
+
+impl Default for Root {
+    #[inline]
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl WireType for Root {
+    const WIRE_TYPE: u8 = WIRE_TYPE_SIZED;
+}
+
+impl Serialize2 for Root {
+    #[inline]
+    fn size(&self) -> u32 {
+        0
+    }
+
+    #[inline]
+    fn serialize(&self, _writer: &mut impl io::Write) -> io::Result<()> {
+        Ok(())
+    }
+}
+
 #[derive(Default, Debug)]
 pub struct Runtime {
-    node: Rc<Node<RuntimeInner>>,
+    node: Rc<Node<Child, Root>>,
 }
 
 impl Runtime {
@@ -47,27 +91,30 @@ impl Runtime {
         Default::default()
     }
 
+    #[inline]
     pub fn nested(&self, tag: u16) -> Self {
         Self {
-            node: Rc::new(Node::child(&self.node, RuntimeInner::new(tag))),
+            node: Rc::new(Node::child(&self.node, Child::new(tag))),
         }
     }
 
+    #[inline]
     pub fn parent(&self) -> Self {
         Self {
             node: self.node.parent().expect("expect a parent `Runtime`"),
         }
     }
 
+    #[inline]
     pub fn clear_cached_size(&self) {
         Self::clear_cached_size_branch(&self.node);
     }
 
-    fn clear_cached_size_branch(node: &Node<RuntimeInner>) {
+    fn clear_cached_size_branch(node: &Node<Child, Root>) {
         match node {
-            Node::Root => (),
-            Node::Child { parent, value, .. } => {
-                value.cached_size.clear();
+            Node::Root { inner } => inner.value().cached_size.clear(),
+            Node::Child { parent, inner } => {
+                inner.value().cached_size.clear();
                 Self::clear_cached_size_branch(parent);
             }
         }
@@ -75,7 +122,7 @@ impl Runtime {
 }
 
 impl WireType for Runtime {
-    const WIRE_TYPE: u8 = <Node<RuntimeInner> as WireType>::WIRE_TYPE;
+    const WIRE_TYPE: u8 = <Node<Child, Root> as WireType>::WIRE_TYPE;
 }
 
 impl Serialize2 for Runtime {
@@ -94,7 +141,7 @@ impl Serialize2 for Runtime {
 mod tests {
     use crate::Serialize2;
 
-    use super::Runtime;
+    use super::{Node, Runtime};
 
     #[test]
     fn test() {
@@ -111,6 +158,13 @@ mod tests {
         println!("{:?}", bytes);
 
         println!("{}", runtime.size());
+        println!("{:?}", runtime);
+
+        match &*runtime.node {
+            Node::Root { inner } => inner.value().cached_size.set(6),
+            Node::Child { inner, .. } => inner.value().cached_size.set(7),
+        }
+
         println!("{:?}", runtime);
 
         runtime.clear_cached_size();
