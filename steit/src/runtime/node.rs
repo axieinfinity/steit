9cad2059adceba21ccc4b56ebce9cb0e69e57ecf
Trung Nguyen <me@trungfinity.com>
2019-11-05 06:02:17 +0700

    Support different types for root and child tree nodes

--- steit/src/runtime/node.rs
+++ steit/src/runtime/node.rs
@@ -8,62 +8,109 @@ use crate::{
 use super::cached_size::CachedSize;
 
 #[derive(Debug)]
-pub enum Node<T> {
-    Root,
-    Child {
-        parent: Rc<Self>,
-        value: T,
-        /// Cached size of the node itself
-        cached_size: CachedSize,
-    },
+pub struct NodeInner<T> {
+    value: T,
+
+    /// Cached size of the current branch, starting from this node's value to root.
+    ///
+    /// Since this tree structure is immutable, this cached size should never be touched.
+    cached_size: CachedSize,
 }
 
-impl<T> Node<T> {
+impl<T> NodeInner<T> {
     #[inline]
-    pub fn child(parent: &Rc<Self>, value: T) -> Self {
-        Node::Child {
-            parent: parent.clone(),
+    pub fn new(value: T) -> Self {
+        Self {
             value,
             cached_size: CachedSize::unset(),
         }
     }
 
     #[inline]
+    pub fn value(&self) -> &T {
+        &self.value
+    }
+}
+
+#[derive(Debug)]
+pub enum Node<Child, Root = Child> {
+    Root {
+        inner: NodeInner<Root>,
+    },
+    Child {
+        parent: Rc<Self>,
+        inner: NodeInner<Child>,
+    },
+}
+
+impl<Child, Root> Node<Child, Root> {
+    #[inline]
+    pub fn root(value: Root) -> Self {
+        Node::Root {
+            inner: NodeInner::new(value),
+        }
+    }
+
+    #[inline]
+    pub fn child(parent: &Rc<Self>, value: Child) -> Self {
+        Node::Child {
+            parent: parent.clone(),
+            inner: NodeInner::new(value),
+        }
+    }
+
+    #[inline]
     pub fn parent(&self) -> Option<Rc<Self>> {
         match self {
-            Node::Root => None,
+            Node::Root { .. } => None,
             Node::Child { parent, .. } => Some(parent.clone()),
         }
     }
 }
 
-impl<T> Default for Node<T> {
+impl<Child, Root: Default> Default for Node<Child, Root> {
     #[inline]
     fn default() -> Self {
-        Node::Root
+        Self::root(Default::default())
     }
 }
 
-impl<T> WireType for Node<T> {
+impl<Child, Root> WireType for Node<Child, Root> {
     const WIRE_TYPE: u8 = WIRE_TYPE_SIZED;
 }
 
-impl<T: Serialize2> Serialize2 for Node<T> {
+impl<Child: Serialize2, Root: Serialize2> Serialize2 for Node<Child, Root> {
     fn size(&self) -> u32 {
         match self {
-            Node::Root => 0,
-            Node::Child {
-                parent,
-                value,
-                cached_size,
-            } => cached_size.get_or_set_from(|| {
-                let mut size = parent.size();
+            Node::Root { inner } => inner.cached_size.get_or_set_from(|| {
+                let mut size = 0;
 
-                if T::WIRE_TYPE == WIRE_TYPE_SIZED {
-                    size += value.size().size();
+                // TODO: Refactor this into a common utility
+                if Root::WIRE_TYPE == WIRE_TYPE_SIZED {
+                    if inner.value.size() > 0 {
+                        size += inner.value.size().size();
+                        size += inner.value.size();
+                    }
+                } else {
+                    // TODO: Check for default varint value
+                    size += inner.value.size();
                 }
 
-                size += value.size();
+                size
+            }),
+
+            Node::Child { parent, inner } => inner.cached_size.get_or_set_from(|| {
+                let mut size = parent.size();
+
+                if Child::WIRE_TYPE == WIRE_TYPE_SIZED {
+                    if inner.value.size() > 0 {
+                        size += inner.value.size().size();
+                        size += inner.value.size();
+                    }
+                } else {
+                    size += inner.value.size();
+                }
+
                 size
             }),
         }
@@ -71,16 +118,31 @@ impl<T: Serialize2> Serialize2 for Node<T> {
 
     fn serialize(&self, writer: &mut impl io::Write) -> io::Result<()> {
         match self {
-            Node::Root => Ok(()),
-            Node::Child { parent, value, .. } => {
+            Node::Root { inner } => {
+                if Root::WIRE_TYPE == WIRE_TYPE_SIZED {
+                    if inner.value.size() > 0 {
+                        inner.value.size().serialize(writer)?;
+                        inner.value.serialize(writer)?;
+                    }
+                } else {
+                    inner.value.serialize(writer)?;
+                }
+            }
+
+            Node::Child { parent, inner, .. } => {
                 parent.serialize(writer)?;
 
-                if T::WIRE_TYPE == WIRE_TYPE_SIZED {
-                    value.size().serialize(writer)?;
+                if Child::WIRE_TYPE == WIRE_TYPE_SIZED {
+                    if inner.value.size() > 0 {
+                        inner.value.size().serialize(writer)?;
+                        inner.value.serialize(writer)?;
+                    }
+                } else {
+                    inner.value.serialize(writer)?;
                 }
-
-                value.serialize(writer)
             }
         }
+
+        Ok(())
     }
 }
