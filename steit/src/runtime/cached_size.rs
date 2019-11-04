774d455912e480e1acbc4185117173af7467ae72
Trung Nguyen <me@trungfinity.com>
2019-11-05 04:47:43 +0700

    Rework CachedSize to use RefCell

--- steit/src/runtime/cached_size.rs
+++ steit/src/runtime/cached_size.rs
@@ -1,39 +1,45 @@
-use std::sync::atomic::{AtomicU32, Ordering};
+use std::cell::RefCell;
 
 #[derive(Debug)]
 pub struct CachedSize {
-    size: AtomicU32,
+    size: RefCell<u32>,
 }
 
 impl CachedSize {
     const UNSET: u32 = -1i8 as u32;
 
     #[inline]
-    pub fn new() -> Self {
+    pub fn unset() -> Self {
         Self {
-            size: AtomicU32::new(Self::UNSET),
+            size: RefCell::new(Self::UNSET),
         }
     }
 
     /// Get cached size
     #[inline]
     pub fn get(&self) -> u32 {
-        self.size.load(Ordering::Relaxed)
+        *self.size.borrow()
+    }
+
+    #[inline]
+    pub fn is_set(&self) -> bool {
+        self.get() != Self::UNSET
     }
 
     /// Set cached size
     #[inline]
     pub fn set(&self, size: u32) {
-        self.size.store(size, Ordering::Relaxed)
+        self.size.replace(size);
     }
 
     #[inline]
     pub fn get_or_set_from(&self, f: impl FnOnce() -> u32) -> u32 {
-        match self.size.load(Ordering::Relaxed) {
+        let size = &mut *self.size.borrow_mut();
+
+        match *size {
             Self::UNSET => {
-                let size = f();
-                self.set(size);
-                size
+                *size = f();
+                *size
             }
             size => size,
         }
@@ -44,28 +50,3 @@ impl CachedSize {
         self.set(Self::UNSET)
     }
 }
-
-impl Default for CachedSize {
-    #[inline]
-    fn default() -> Self {
-        Self::new()
-    }
-}
-
-impl Clone for CachedSize {
-    #[inline]
-    fn clone(&self) -> Self {
-        Self {
-            size: AtomicU32::new(self.get()),
-        }
-    }
-}
-
-impl PartialEq for CachedSize {
-    #[inline]
-    fn eq(&self, _other: &Self) -> bool {
-        true
-    }
-}
-
-impl Eq for CachedSize {}
--- steit/src/runtime/mod.rs
+++ steit/src/runtime/mod.rs
@@ -9,5 +9,4 @@ pub use runtime::Runtime;
 mod cached_size;
 mod runtime2;
 
-pub use cached_size::CachedSize;
 pub use runtime2::Runtime as Runtime2;
