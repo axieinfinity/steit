9d39e7ab388bfd0233de1e5f28c4022bd8763e4e
Trung Nguyen <me@trungfinity.com>
2019-11-06 00:13:40 +0700

    Reimplement and improve Attr a lot

---
+++ steit-derive/src/derive2/attr.rs
@@ -0,0 +1,118 @@
+use proc_macro2::TokenStream;
+use quote::ToTokens;
+
+use crate::derive2::ctx::Context;
+
+pub struct Attr<'a, T> {
+    context: &'a Context,
+    name: &'static str,
+    tokens: TokenStream,
+    value: Option<T>,
+}
+
+impl<'a, T> Attr<'a, T> {
+    pub fn parse_name_value(
+        &mut self,
+        meta: &syn::MetaNameValue,
+        f: &mut impl FnMut(&syn::Lit) -> Result<T, &'a str>,
+    ) -> bool {
+        if meta.path.is_ident(self.name) {
+            match f(&meta.lit) {
+                Ok(value) => self.set(meta, value),
+                Err(ty) => self.context.error(
+                    &meta.lit,
+                    format!("expected `{}` attribute to be {}", self.name, ty),
+                ),
+            }
+
+            true
+        } else {
+            false
+        }
+    }
+
+    pub fn new(context: &'a Context, name: &'static str) -> Self {
+        Self {
+            context,
+            name,
+            tokens: TokenStream::new(),
+            value: None,
+        }
+    }
+
+    pub fn set(&mut self, tokens: impl ToTokens, value: T) {
+        if self.value.is_some() {
+            self.context
+                .error(tokens, format!("duplicate steit attribute `{}`", self.name));
+        } else {
+            self.tokens = tokens.to_token_stream();
+            self.value = Some(value);
+        }
+    }
+
+    pub fn get(self) -> Option<T> {
+        self.value
+    }
+}
+
+impl Attr<'_, bool> {
+    pub fn parse_path(&mut self, path: &syn::Path) -> bool {
+        if path.is_ident(self.name) {
+            self.set(path, true);
+            true
+        } else {
+            false
+        }
+    }
+
+    pub fn parse_bool(&mut self, meta: &syn::MetaNameValue) -> bool {
+        self.parse_name_value(meta, &mut |lit| match lit {
+            syn::Lit::Bool(lit) => Ok(lit.value),
+            _ => Err("a bool"),
+        })
+    }
+}
+
+pub fn parse(
+    attrs: &mut Vec<syn::Attribute>,
+    context: &Context,
+    f: &mut impl FnMut(&syn::Meta) -> bool,
+) {
+    // Retain only non-steit attributes
+    attrs.retain(|attr| {
+        if !attr.path.is_ident("steit") {
+            return true;
+        }
+
+        let meta_list = match attr.parse_meta() {
+            Ok(syn::Meta::List(meta)) => meta.nested,
+
+            Ok(other) => {
+                context.error(other, "expected #[steit(...)]");
+                return false;
+            }
+
+            Err(error) => {
+                context.syn_error(error);
+                return false;
+            }
+        };
+
+        for meta in &meta_list {
+            match meta {
+                syn::NestedMeta::Meta(meta) if f(meta) => (),
+
+                syn::NestedMeta::Meta(item) => {
+                    let path = item.path().to_token_stream().to_string().replace(' ', "");
+                    context.error(item.path(), format!("unknown steit attribute `{}`", path));
+                }
+
+                syn::NestedMeta::Lit(lit) => {
+                    context.error(lit, "unexpected literal in steit attributes")
+                }
+            }
+        }
+
+        false
+    })
+}
