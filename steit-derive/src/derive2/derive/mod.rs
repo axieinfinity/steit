d7b1cf1852d101fa248a3fc29b4040da189238a2
Trung Nguyen <me@trungfinity.com>
2019-11-07 16:47:09 +0700

    Refactor the new derivation skeleton

--- steit-derive/src/derive2/derive/mod.rs
+++ steit-derive/src/derive2/derive/mod.rs
@@ -2,13 +2,14 @@ use proc_macro2::TokenStream;
 use quote::ToTokens;
 
 use crate::derive2::{
-    attr::{self, Attr},
+    attr::{Attr, AttrParse},
     ctx::Context,
     r#impl::Impl,
     string,
 };
 
 mod r#enum;
+mod field;
 mod r#struct;
 mod union;
 mod variant;
@@ -33,7 +34,7 @@ impl DeriveAttrs {
     pub fn parse(context: &Context, args: syn::AttributeArgs) -> Self {
         let mut own_crate = Attr::new(context, "own_crate");
 
-        attr::parse_meta_list(context, args, &mut |meta| match meta {
+        args.parse(context, &mut |meta| match meta {
             syn::Meta::Path(path) if own_crate.parse_path(path) => true,
             syn::Meta::NameValue(meta) if own_crate.parse_bool(meta) => true,
             _ => false,
@@ -52,32 +53,43 @@ pub fn derive(
 ) -> TokenStream {
     let context = Context::new();
     let attrs = DeriveAttrs::parse(&context, args);
-    let r#impl = Impl::new(&input);
+    let r#impl = Impl::new(&input.ident, &input.generics);
 
     let output = match &mut input.data {
-        syn::Data::Enum(data) => Enum::new(&derive, &context, &r#impl, data).into_token_stream(),
+        syn::Data::Enum(data) => Enum::parse(&derive, &context, &r#impl, data)
+            .ok()
+            .into_token_stream(),
 
-        syn::Data::Struct(data) => {
-            Struct::parse(&derive, &context, &r#impl, &mut data.fields, None)
-                .ok()
-                .into_token_stream()
-        }
+        syn::Data::Struct(data) => Struct::parse(
+            &derive,
+            &context,
+            &r#impl,
+            &mut input.attrs,
+            &mut data.fields,
+            None,
+        )
+        .ok()
+        .into_token_stream(),
 
-        syn::Data::Union(data) => {
-            Union::parse(&derive, &context, &r#impl, data).into_token_stream()
-        }
+        syn::Data::Union(data) => Union::parse(&derive, &context, &r#impl, data)
+            .ok()
+            .into_token_stream(),
     };
 
-    if let Err(errors) = context.check() {
+    let output = if let Err(errors) = context.check() {
         to_compile_errors(errors)
     } else {
-        let output = wrap_in_const(&derive, &input.ident, attrs.own_crate, output);
+        wrap_in_const(&derive, &input.ident, attrs.own_crate, output)
+    };
 
-        quote! {
-            #input
-            #output
-        }
-    }
+    let result = quote! {
+        #input
+        #output
+    };
+
+    println!("{}", result.to_string());
+
+    result
 }
 
 fn to_compile_errors(errors: Vec<syn::Error>) -> TokenStream {
@@ -91,8 +103,6 @@ fn wrap_in_const(
     own_crate: bool,
     tokens: TokenStream,
 ) -> TokenStream {
-    println!("{}", tokens.to_string());
-
     let dummy_const = format_ident!(
         "_IMPL_{}_FOR_{}",
         match derive {
@@ -106,19 +116,14 @@ fn wrap_in_const(
     let (extern_crate, krate) = if own_crate {
         (quote!(), quote!(crate))
     } else {
-        (quote!(extern crate steit), quote!(steit))
+        (quote! { extern crate steit; }, quote!(steit))
     };
 
     quote! {
         const #dummy_const: () = {
-            #extern_crate;
-
+            #extern_crate
             use std::io::{self, Read};
-
-            // We don't import `Varint` directly
-            // to avoid confusing `serialize` and `deserialize` calls.
-            use #krate::{iowrap, varint, Deserialize, RawEntryKind, Runtime, Serialize};
-
+            use #krate::{Runtime2, Serialize2, WireType};
             #tokens
         };
     }
