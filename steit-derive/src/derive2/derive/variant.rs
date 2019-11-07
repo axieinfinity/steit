3b8accdceb3c84c8ec4499a970df70c04ffe059d
Trung Nguyen <me@trungfinity.com>
2019-11-08 00:40:24 +0700

    Return unknown attributes from Variant back to Struct

--- steit-derive/src/derive2/derive/variant.rs
+++ steit-derive/src/derive2/derive/variant.rs
@@ -1,3 +1,5 @@
+use proc_macro2::TokenStream;
+
 use crate::derive2::{
     attr::{Attr, AttrParse},
     ctx::Context,
@@ -6,20 +8,25 @@ use crate::derive2::{
 
 struct VariantAttrs {
     tag: u16,
+    tag_tokens: TokenStream,
 }
 
 impl VariantAttrs {
-    pub fn parse(context: &Context, attrs: &mut Vec<syn::Attribute>) -> derive::Result<Self> {
+    pub fn parse(
+        context: &Context,
+        variant: &mut syn::Variant,
+    ) -> derive::Result<(Self, syn::AttributeArgs)> {
         let mut tag = Attr::new(context, "tag");
 
-        attrs.parse(context, &mut |meta| match meta {
+        let unknown_attrs = (&mut variant.attrs).parse(context, false, &mut |meta| match meta {
             syn::Meta::NameValue(meta) if tag.parse_int(meta) => true,
             _ => false,
         });
 
-        if let Some(tag) = tag.get() {
-            Ok(Self { tag })
+        if let Some((tag, tag_tokens)) = tag.get_with_tokens() {
+            Ok((Self { tag, tag_tokens }, unknown_attrs))
         } else {
+            context.error(variant, "expected a valid tag #[steit(tag = ...)]");
             Err(())
         }
     }
@@ -31,10 +38,22 @@ pub struct Variant {
 }
 
 impl Variant {
-    pub fn parse(context: &Context, variant: &mut syn::Variant) -> derive::Result<Self> {
-        VariantAttrs::parse(context, &mut variant.attrs).map(|attrs| Self {
-            ident: variant.ident.clone(),
-            attrs,
+    pub fn parse(
+        context: &Context,
+        variant: &mut syn::Variant,
+    ) -> derive::Result<(Self, syn::AttributeArgs)> {
+        VariantAttrs::parse(context, variant).map(|(attrs, unknown_attrs)| {
+            (
+                Self {
+                    ident: variant.ident.clone(),
+                    attrs,
+                },
+                unknown_attrs,
+            )
         })
+    }
+
+    pub fn tag_with_tokens(&self) -> (u16, &TokenStream) {
+        (self.attrs.tag, &self.attrs.tag_tokens)
     }
 }
