7cc03751c461797e16444d2988d0ac7554c9c19b
Trung Nguyen <me@trungfinity.com>
2019-11-08 18:39:22 +0700

    Move set_runtime and with_runtime from State to Deserialize

--- steit/src/de2.rs
+++ steit/src/de2.rs
@@ -5,10 +5,20 @@ use iowrap::Eof;
 use crate::{
     varint::Varint,
     wire_type::{WireType, WIRE_TYPE_SIZED, WIRE_TYPE_VARINT},
+    Runtime2,
 };
 
 pub trait Deserialize: Default + WireType {
+    fn set_runtime(&mut self, runtime: Runtime2) -> &mut Self;
     fn merge(&mut self, reader: &mut Eof<impl io::Read>) -> io::Result<()>;
+
+    #[inline]
+    fn with_runtime(runtime: Runtime2) -> Self {
+        // We use `Self::` since surprisingly `Default::` leaves us with an unknown type.
+        let mut state = Self::default();
+        state.set_runtime(runtime);
+        state
+    }
 
     #[inline]
     fn merge_nested(&mut self, reader: &mut Eof<impl io::Read>) -> io::Result<()> {
@@ -24,7 +34,7 @@ pub trait Deserialize: Default + WireType {
 
     #[inline]
     fn deserialize(reader: &mut Eof<impl io::Read>) -> io::Result<Self> {
-        // We use `Self::` since surprisingly `Default::` leaves us with an unknown type.
+        // We use `Self::` because of the same reason as in `with_runtime`.
         let mut value = Self::default();
         value.merge(reader)?;
         Ok(value)
@@ -32,6 +42,11 @@ pub trait Deserialize: Default + WireType {
 }
 
 impl<T: Default + Varint + WireType> Deserialize for T {
+    #[inline]
+    fn set_runtime(&mut self, runtime: Runtime2) -> &mut Self {
+        self
+    }
+
     #[inline]
     fn merge(&mut self, reader: &mut Eof<impl io::Read>) -> io::Result<()> {
         *self = Varint::deserialize(reader)?;
@@ -44,7 +59,6 @@ impl<T: Default + Varint + WireType> Deserialize for T {
     }
 }
 
-#[inline]
 pub fn exhaust_nested(tag: u16, wire_type: u8, reader: &mut Eof<impl io::Read>) -> io::Result<()> {
     match wire_type {
         WIRE_TYPE_VARINT => {
--- steit/src/state2.rs
+++ steit/src/state2.rs
@@ -1,13 +1,3 @@
 use crate::{Deserialize2, Runtime2, Serialize2};
 
-pub trait State: Serialize2 + Deserialize2 {
-    #[inline]
-    fn with_runtime(runtime: Runtime2) -> Self {
-        // See `Deserialize` on why we use `Self::` instead of `Default::`
-        let mut state = Self::default();
-        state.set_runtime(runtime);
-        state
-    }
-
-    fn set_runtime(&mut self, runtime: Runtime2) -> &mut Self;
-}
+pub trait State: Serialize2 + Deserialize2 {}
