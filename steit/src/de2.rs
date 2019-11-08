64e38c0bdf1ce439bcceeaad5447d2d3f68413ac
Trung Nguyen <me@trungfinity.com>
2019-11-08 12:59:06 +0700

    Support exhausting unknown nested fields

--- steit/src/de2.rs
+++ steit/src/de2.rs
@@ -43,3 +43,29 @@ impl<T: Default + Varint + WireType> Deserialize for T {
         Varint::deserialize(reader)
     }
 }
+
+#[inline]
+pub fn exhaust_nested(tag: u16, wire_type: u8, reader: &mut Eof<impl io::Read>) -> io::Result<()> {
+    match wire_type {
+        WIRE_TYPE_VARINT => {
+            // TODO: Remove `as Deserialize` after refactoring `Varint`
+            <u8 as Deserialize>::deserialize(reader)?;
+        }
+
+        WIRE_TYPE_SIZED => {
+            // TODO: Remove `as Deserialize` after refactoring `Varint`
+            let size = <u64 as Deserialize>::deserialize(reader)?;
+            let mut buf = Vec::new();
+            reader.by_ref().take(size).read_to_end(&mut buf)?;
+        }
+
+        _ => {
+            return Err(io::Error::new(
+                io::ErrorKind::InvalidData,
+                format!("unexpected tag {} or wire type {}", tag, wire_type),
+            ))
+        }
+    }
+
+    Ok(())
+}
