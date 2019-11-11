c29d6787a89f50db5379b67665b27d3d5a768948
Trung Nguyen <me@trungfinity.com>
2019-11-12 02:44:18 +0700

    Deserialize replay bytes directly, not as Vec<impl Varint>

--- steit/src/rt/replay.rs
+++ steit/src/rt/replay.rs
@@ -1,7 +1,29 @@
+use std::io::{self, Read};
+
+use iowrap::Eof;
+
+use crate::{wire_type::WIRE_TYPE_SIZED, Merge, WireType};
+
 pub enum ReplayKind {
     Update,
     Add,
     Remove,
+}
+
+#[derive(Default, Debug)]
+pub struct Bytes {
+    bytes: Vec<u8>,
+}
+
+impl WireType for Bytes {
+    const WIRE_TYPE: u8 = WIRE_TYPE_SIZED;
+}
+
+impl Merge for Bytes {
+    fn merge(&mut self, reader: &mut Eof<impl io::Read>) -> io::Result<()> {
+        reader.read_to_end(&mut self.bytes)?;
+        Ok(())
+    }
 }
 
 #[crate::steitize(Deserialize, own_crate, no_runtime)]
@@ -12,14 +34,14 @@ pub enum ReplayEntry {
         #[steit(tag = 0)]
         path: Vec<u16>,
         #[steit(tag = 1)]
-        value: Vec<u8>,
+        value: Bytes,
     },
     #[steit(tag = 1)]
     Add {
         #[steit(tag = 0)]
         path: Vec<u16>,
         #[steit(tag = 1)]
-        item: Vec<u8>,
+        item: Bytes,
     },
     #[steit(tag = 2)]
     Remove {
@@ -31,8 +53,8 @@ pub enum ReplayEntry {
 impl ReplayEntry {
     pub fn decompose(self) -> (Vec<u16>, ReplayKind, Vec<u8>) {
         match self {
-            ReplayEntry::Update { path, value } => (path, ReplayKind::Update, value),
-            ReplayEntry::Add { path, item } => (path, ReplayKind::Add, item),
+            ReplayEntry::Update { path, value } => (path, ReplayKind::Update, value.bytes),
+            ReplayEntry::Add { path, item } => (path, ReplayKind::Add, item.bytes),
             ReplayEntry::Remove { path } => (path, ReplayKind::Remove, Vec::new()),
         }
     }
