712ca4f81989f35a091fbef938a84a595bd682ba
Trung Nguyen <me@trungfinity.com>
2019-11-12 14:19:50 +0700

    Implement Bytes type properly

---
+++ steit/src/types/bytes.rs
@@ -0,0 +1,61 @@
+use std::{
+    fmt,
+    io::{self, Read},
+};
+
+use iowrap::Eof;
+
+use crate::{
+    wire_type::{WireType, WIRE_TYPE_SIZED},
+    Merge, Serialize,
+};
+
+#[derive(Default)]
+pub struct Bytes {
+    bytes: Vec<u8>,
+}
+
+impl Bytes {
+    #[inline]
+    pub fn new(value: &impl Serialize) -> Self {
+        let mut bytes = Vec::new();
+        value.serialize(&mut bytes).unwrap();
+        Self { bytes }
+    }
+
+    #[inline]
+    pub fn bytes(self) -> Vec<u8> {
+        self.bytes
+    }
+}
+
+impl fmt::Debug for Bytes {
+    #[inline]
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "Bytes {:?}", self.bytes)
+    }
+}
+
+impl WireType for Bytes {
+    const WIRE_TYPE: u8 = WIRE_TYPE_SIZED;
+}
+
+impl Serialize for Bytes {
+    #[inline]
+    fn size(&self) -> u32 {
+        self.bytes.size()
+    }
+
+    #[inline]
+    fn serialize(&self, writer: &mut impl io::Write) -> io::Result<()> {
+        self.bytes.serialize(writer)
+    }
+}
+
+impl Merge for Bytes {
+    #[inline]
+    fn merge(&mut self, reader: &mut Eof<impl io::Read>) -> io::Result<()> {
+        reader.read_to_end(&mut self.bytes)?;
+        Ok(())
+    }
+}
