b66eb5884f657849a55fef5c69fb52579fb34c4e
Trung Nguyen <me@trungfinity.com>
2019-12-01 03:47:54 +0700

    Add some tests for CachedSize

--- steit/src/rt/cached_size.rs
+++ steit/src/rt/cached_size.rs
@@ -3,7 +3,11 @@ use std::{
     sync::atomic::{AtomicU32, Ordering},
 };
 
-// Reference: https://bit.ly/2XhnF25
+/// Cached object size to prevent duplicate size calculation in serialization.
+///
+/// It is always equal to itself so the containing object can use `#[derive(Eq)]`.
+///
+/// Reference: https://github.com/stepancheg/rust-protobuf/blob/68c7a5a/protobuf/src/cached_size.rs
 #[derive(Default, Debug)]
 pub struct CachedSize {
     size: AtomicU32,
@@ -15,13 +19,21 @@ impl CachedSize {
         Self::default()
     }
 
-    /// Get cached size
+    /// Gets cached size.
+    ///
+    /// ```
+    /// use steit::CachedSize;
+    ///
+    /// let cached_size = CachedSize::new();
+    /// cached_size.set(1337);
+    /// assert_eq!(cached_size.get(), 1337);
+    /// ```
     #[inline]
     pub fn get(&self) -> u32 {
         self.size.load(Ordering::Relaxed) as u32
     }
 
-    /// Set cached size
+    /// Sets cached size.
     #[inline]
     pub fn set(&self, size: u32) {
         self.size.store(size, Ordering::Relaxed);
