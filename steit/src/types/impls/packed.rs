1ee15698a3164aeabbbeda5ca70ffec0976ca57b
Trung Nguyen <me@trungfinity.com>
2019-12-01 04:02:51 +0700

    Add some tests for Vec<impl Varint> (packed) impls

--- steit/src/types/impls/packed.rs
+++ steit/src/types/impls/packed.rs
@@ -11,6 +11,16 @@ impl<T: Varint> WireType for Vec<T> {
 }
 
 impl<T: Varint> Serialize for Vec<T> {
+    /// Computes serialized size for `Vec<impl Varint>`.
+    ///
+    /// ```
+    /// use steit::Serialize;
+    ///
+    /// assert_eq!(Vec::<u8>::new().compute_size(), 0);
+    /// assert_eq!(vec![0].compute_size(), 1);
+    /// assert_eq!(vec![0, 0, 0].compute_size(), 3);
+    /// assert_eq!(vec![1337].compute_size(), 2);
+    /// ```
     #[inline]
     fn compute_size(&self) -> u32 {
         let mut size = 0;
@@ -22,6 +32,23 @@ impl<T: Varint> Serialize for Vec<T> {
         size
     }
 
+    /// Serializes `Vec<impl Varint>`.
+    ///
+    /// ```
+    /// use steit::Serialize;
+    ///
+    /// let mut bytes = Vec::new();
+    /// Vec::<u8>::new().serialize_with_cached_size(&mut bytes).unwrap();
+    /// assert_eq!(&bytes, &[]);
+    ///
+    /// let mut bytes = Vec::new();
+    /// vec![0, 0, 0].serialize_with_cached_size(&mut bytes).unwrap();
+    /// assert_eq!(&bytes, &[0, 0, 0]);
+    ///
+    /// let mut bytes = Vec::new();
+    /// vec![1337].serialize_with_cached_size(&mut bytes).unwrap();
+    /// assert_eq!(&bytes, &[242, 20]);
+    /// ```
     #[inline]
     fn serialize_with_cached_size(&self, writer: &mut impl io::Write) -> io::Result<()> {
         for item in self {
@@ -33,6 +60,22 @@ impl<T: Varint> Serialize for Vec<T> {
 }
 
 impl<T: Varint> Merge for Vec<T> {
+    /// Merges a serialized value with an existing `Vec<impl Varint>`.
+    ///
+    /// ```
+    /// use steit::{Merge, Eof};
+    ///
+    /// let mut value = Vec::<i32>::new();
+    ///
+    /// value.merge(&mut Eof::new([1].as_ref())).unwrap();
+    /// assert_eq!(&value, &[-1]);
+    ///
+    /// value.merge(&mut Eof::new([].as_ref())).unwrap();
+    /// assert_eq!(&value, &[-1]);
+    ///
+    /// value.merge(&mut Eof::new([0, 242, 20, 0, 3, 0].as_ref())).unwrap();
+    /// assert_eq!(&value, &[-1, 0, 1337, 0, -2, 0]);
+    /// ```
     #[inline]
     fn merge(&mut self, reader: &mut Eof<impl io::Read>) -> io::Result<()> {
         while !reader.eof()? {
