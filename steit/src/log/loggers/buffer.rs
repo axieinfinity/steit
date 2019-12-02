21f4e38b346a71c47119ed72d1647133963ca87c
Trung Nguyen <me@trungfinity.com>
2019-12-02 22:54:30 +0700

    Implement some utility methods for BufferLogger

--- steit/src/log/loggers/buffer.rs
+++ steit/src/log/loggers/buffer.rs
@@ -1,6 +1,9 @@
 use std::io;
 
-use crate::log::{LogEntry, Logger};
+use crate::{
+    log::{LogEntry, Logger},
+    Serialize,
+};
 
 pub struct BufferLogger {
     entries: Vec<LogEntry>,
@@ -12,6 +15,31 @@ impl BufferLogger {
         Self {
             entries: Vec::new(),
         }
+    }
+
+    pub fn bytes(&self) -> Vec<u8> {
+        let mut bytes = Vec::new();
+
+        for entry in &self.entries {
+            entry.compute_size();
+            entry
+                .serialize_nested_with_cached_size(None, &mut bytes)
+                .unwrap();
+        }
+
+        bytes
+    }
+
+    #[inline]
+    pub fn clear(&mut self) {
+        self.entries.clear();
+    }
+
+    #[inline]
+    pub fn pluck(&mut self) -> Vec<u8> {
+        let bytes = self.bytes();
+        self.clear();
+        bytes
     }
 }
 
