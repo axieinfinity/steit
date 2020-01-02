8c544ec90d9a997fb0cbb942e4fed50d27e20144
Trung Nguyen <me@trungfinity.com>
2020-01-02 14:41:27 +0700

    Implement HasFieldType for primitive and state types

--- steit/src/gen/gen_meta.rs
+++ steit/src/gen/gen_meta.rs
@@ -52,3 +52,29 @@ pub enum FieldType {
 pub trait HasMeta {
     const META: &'static Meta;
 }
+
+pub trait HasFieldType {
+    const FIELD_TYPE: &'static FieldType;
+}
+
+macro_rules! impl_has_field_type {
+    ($t:ty, $t_name:expr) => {
+        impl HasFieldType for $t {
+            const FIELD_TYPE: &'static FieldType = &FieldType::Primitive($t_name);
+        }
+    };
+}
+
+impl_has_field_type!(u8, "u8");
+impl_has_field_type!(u16, "u16");
+impl_has_field_type!(u32, "u32");
+impl_has_field_type!(u64, "u64");
+impl_has_field_type!(i8, "i8");
+impl_has_field_type!(i16, "i16");
+impl_has_field_type!(i32, "i32");
+impl_has_field_type!(i64, "i64");
+impl_has_field_type!(bool, "bool");
+
+impl<T: HasMeta> HasFieldType for T {
+    const FIELD_TYPE: &'static FieldType = &FieldType::Meta(T::META);
+}
