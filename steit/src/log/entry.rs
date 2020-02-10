d8b2a92d2cbaf9926cbe654d46f938eb8d52f7e8
Nhat Quang <nhatquangsin@gmail.com>
2020-02-10 19:02:03 +0700

    Implement Send + Sync for Logger, Using Arc for LogEntry

--- steit/src/log/entry.rs
+++ steit/src/log/entry.rs
@@ -1,4 +1,4 @@
-use std::rc::Rc;
+use std::sync::Arc;
 
 use crate::{node::Node, types::Bytes, CachedSize, Serialize};
 
@@ -9,27 +9,27 @@ pub enum LogEntry {
     #[steit(tag = 0)]
     Update {
         #[steit(tag = 0)]
-        path: Rc<Node<u16>>,
+        path: Arc<Node<u16>>,
         #[steit(tag = 1)]
         value: Bytes,
     },
     #[steit(tag = 1)]
     Add {
         #[steit(tag = 0)]
-        path: Rc<Node<u16>>,
+        path: Arc<Node<u16>>,
         #[steit(tag = 1)]
         item: Bytes,
     },
     #[steit(tag = 2)]
     Remove {
         #[steit(tag = 0)]
-        path: Rc<Node<u16>>,
+        path: Arc<Node<u16>>,
     },
 }
 
 impl LogEntry {
     #[inline]
-    pub fn new_update(path: Rc<Node<u16>>, value: &impl Serialize) -> Self {
+    pub fn new_update(path: Arc<Node<u16>>, value: &impl Serialize) -> Self {
         LogEntry::Update {
             path,
             value: Bytes::with_value(value),
@@ -38,7 +38,7 @@ impl LogEntry {
     }
 
     #[inline]
-    pub fn new_add(path: Rc<Node<u16>>, item: &impl Serialize) -> Self {
+    pub fn new_add(path: Arc<Node<u16>>, item: &impl Serialize) -> Self {
         LogEntry::Add {
             path,
             item: Bytes::with_value(item),
@@ -47,7 +47,7 @@ impl LogEntry {
     }
 
     #[inline]
-    pub fn new_remove(path: Rc<Node<u16>>) -> Self {
+    pub fn new_remove(path: Arc<Node<u16>>) -> Self {
         LogEntry::Remove {
             path,
             cached_size: CachedSize::new(),
@@ -55,7 +55,7 @@ impl LogEntry {
     }
 
     #[inline]
-    pub fn path(&self) -> &Rc<Node<u16>> {
+    pub fn path(&self) -> &Arc<Node<u16>> {
         match self {
             LogEntry::Update { path, .. } => path,
             LogEntry::Add { path, .. } => path,
