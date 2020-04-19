3b76d930830ff2d729652f9f94e772804bed90de
Trung Nguyen <me@ngthanhtrung.com>
2020-04-20 03:09:37 +0700

    Add separate test utilities for new code

---
+++ steit/src/test_util_v2.rs
@@ -0,0 +1,112 @@
+use std::fmt;
+
+use super::ser_v2::{SerializeNested, SerializeOmissible, SerializeV2};
+
+// #[macro_export]
+// macro_rules! test_case {
+//     ($name:ident : $assert:expr ; $($args:expr),+) => {
+//         #[test]
+//         fn $name() {
+//             $assert($($args),+);
+//         }
+//     };
+//
+//     ($name:ident : $assert:expr ; $($input:expr),+ => $($output:expr),+) => {
+//         test_case!($name : $assert ; $($input),+, $($output),+);
+//     };
+// }
+//
+// #[steitize(Serialize, Deserialize, own_crate)]
+// #[derive(PartialEq, Debug)]
+// pub struct Foo(#[steit(tag = 0)] i32, #[steit(tag = 1)] i32);
+//
+// impl Foo {
+//     pub fn with(f0: i32, f1: i32) -> Self {
+//         Self {
+//             0: f0,
+//             1: f1,
+//             ..Foo::new()
+//         }
+//     }
+// }
+//
+// #[steitize(State, own_crate)]
+// #[derive(PartialEq, Debug)]
+// pub struct Point {
+//     #[steit(tag = 0)]
+//     x: i32,
+//     #[steit(tag = 1)]
+//     y: i32,
+//     #[steit(tag = 2)]
+//     z: i32,
+// }
+//
+// impl Point {
+//     pub fn with(runtime: Runtime, x: i32, y: i32, z: i32) -> Self {
+//         Self {
+//             x,
+//             y,
+//             z,
+//             ..Point::new(runtime)
+//         }
+//     }
+//
+//     pub fn x(&self) -> i32 {
+//         self.x
+//     }
+// }
+
+pub fn assert_size<T: SerializeV2>(value: T, size: u32) {
+    assert_eq!(value.compute_size(), size);
+}
+
+pub fn serialize<T: SerializeV2>(value: T) -> Vec<u8> {
+    let mut bytes = Vec::new();
+    value.serialize(&mut bytes).unwrap();
+    bytes
+}
+
+pub fn assert_serialize<T: SerializeV2>(value: T, bytes: &[u8]) {
+    assert_eq!(&*serialize(value), bytes);
+}
+
+pub fn serialize_nested(value: impl SerializeOmissible, tag: impl Into<Option<u32>>) -> Vec<u8> {
+    let mut bytes = Vec::new();
+    value.cache_size();
+    value.serialize_nested(tag, true, &mut bytes).unwrap();
+    bytes
+}
+
+pub fn assert_serialize_nested(
+    value: impl SerializeOmissible,
+    tag: impl Into<Option<u32>>,
+    bytes: &[u8],
+) {
+    assert_eq!(&*serialize_nested(value, tag), bytes);
+}
+
+// pub fn merge<T: Merge>(mut value: T, bytes: &[u8]) -> T {
+//     value.merge(&mut Eof::new(bytes)).unwrap();
+//     value
+// }
+//
+// pub fn assert_merge<T: PartialEq + fmt::Debug + Merge>(value: T, bytes: &[u8], expected_value: T) {
+//     assert_eq!(merge(value, bytes), expected_value);
+// }
+//
+// pub fn deserialize<T: Deserialize>(bytes: &[u8]) -> T {
+//     T::deserialize(&mut Eof::new(bytes)).unwrap()
+// }
+//
+// pub fn assert_deserialize<T: PartialEq + fmt::Debug + Deserialize>(bytes: &[u8], value: T) {
+//     assert_eq!(deserialize::<T>(bytes), value);
+// }
+//
+// pub fn assert_ser_de<T: Clone + PartialEq + fmt::Debug + Serialize + Deserialize>(value: T) {
+//     assert_eq!(deserialize::<T>(&*serialize(value.clone())), value);
+// }
+//
+// pub fn replay<T: State>(mut value: T, bytes: &[u8]) -> T {
+//     value.replay(&mut Eof::new(bytes)).unwrap();
+//     value
+// }
