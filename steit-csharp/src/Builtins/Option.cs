289935a211f112ac7fb816868430213bbe714497
Trung Nguyen <me@trungfinity.com>
2020-06-02 01:43:57 +0700

    Make Option in C# not abstract

--- steit-csharp/src/Builtins/Option.cs
+++ steit-csharp/src/Builtins/Option.cs
@@ -4,22 +4,40 @@ using Steit.Codec;
 using Steit.State;
 
 namespace Steit.Builtins {
-    public abstract class Option<T> : IState {
-        public Path Path { get; protected set; }
+    public sealed class Option<T> : IState {
+        public Path Path { get; }
 
-        // public static Option<T> Deserialize(IReader reader, Path? path = null) {
+        public bool IsSome { get; }
+        public bool IsNone { get { return !this.IsSome; } }
+
+        public T ValueOrDefault { get; }
+
+        public Option(Path path = null) {
+            this.Path = path ?? Path.Root;
+        }
+
+        public Option(Path path, T value) : this(path) {
+            this.IsSome = true;
+            this.ValueOrDefault = value;
+        }
+
+        public static Option<T> None(Path path = null) {
+            return new Option<T>(path);
+        }
+
+        public static Option<T> Some(Path path, T value) {
+            return new Option<T>(path, value);
+        }
+
         public static Option<T> Deserialize(IReader reader, Path path = null) {
-            path = path ?? Path.Root;
-
             if (!reader.EndOfStream()) {
-                return new Some<T>(path, StateFactory.DeserializeNested<T>(reader, path, 0));
+                return Some(path, StateFactory.DeserializeNested<T>(reader, path, 0));
             } else {
-                return new None<T>(path);
+                return None(path);
             }
         }
 
         public WireType? GetWireType(UInt32 tag) { return null; }
-        // public IState? GetNested(UInt32 tag) { return null; }
         public IState GetNested(UInt32 tag) { return null; }
 
         public void ReplaceAt(UInt32 tag, WireType wireType, IReader reader, bool shouldNotify) {
@@ -29,28 +47,13 @@ namespace Steit.Builtins {
         public void ReplayListPush(IReader reader) { throw new NotSupportedException(); }
         public void ReplayListPop() { throw new NotSupportedException(); }
         public void ReplayMapRemove(UInt32 key) { throw new NotSupportedException(); }
-    }
-
-    public sealed class Some<T> : Option<T> {
-        public T Value { get; }
-
-        public Some(Path path, T value) {
-            this.Path = path;
-            this.Value = value;
-        }
 
         public override string ToString() {
-            return String.Format("Some({0})", this.Value);
-        }
-    }
-
-    public sealed class None<T> : Option<T> {
-        public None(Path path) {
-            this.Path = path;
-        }
-
-        public override string ToString() {
-            return "None";
+            if (this.IsSome) {
+                return String.Format("Some({0})", this.ValueOrDefault);
+            } else {
+                return "None";
+            }
         }
     }
 }
