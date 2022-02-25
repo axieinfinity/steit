2c682af8ec8deea8b30fb8c7785931a5c8376271
Truong Minh Nhat Quang <nhatquangsin@gmail.com>
2022-02-25 09:04:22 +0700

    Add vector (#2)
    * Add statelist
    
    * Add maybe
    
    * Add vector

--- steit-go/pkg/codec/wiretype.go
+++ steit-go/pkg/codec/wiretype.go
@@ -1,8 +1,21 @@
 package codec
 
+import "fmt"
+
 type WireType = int
 
 const (
 	WireTypeSized  WireType = 2
 	WireTypeVarint WireType = 0
 )
+
+func NewWireType(value uint32) WireType {
+	switch value {
+	case uint32(WireTypeVarint):
+		return WireType(value)
+	case uint32(WireTypeSized):
+		return WireType(value)
+	default:
+		panic(fmt.Sprintf("Invalid wire type value: %d", value))
+	}
+}
---
+++ steit-go/pkg/collections/vector.go
@@ -0,0 +1,76 @@
+package collections
+
+import (
+	"github.com/axieinfinity/steit-go/pkg/codec"
+	"github.com/axieinfinity/steit-go/pkg/eventhandler"
+	"github.com/axieinfinity/steit-go/pkg/path"
+	pathpkg "github.com/axieinfinity/steit-go/pkg/path"
+	readerpkg "github.com/axieinfinity/steit-go/pkg/reader"
+	statepkg "github.com/axieinfinity/steit-go/pkg/state"
+)
+
+var _ statepkg.IState = (*Vector)(nil)
+
+type Vector struct {
+	path     *pathpkg.Path
+	items    []interface{}
+	count    int
+}
+
+func NewVector(path *pathpkg.Path, items []interface{}) Vector {
+	vector := Vector{}
+
+	if path != nil {
+		vector.path = path
+	} else {
+		vector.path = pathpkg.Root
+	}
+
+	if len(items) > 0 {
+		vector.items = items
+		vector.count = len(items)
+	}
+
+	return vector
+}
+
+func (v *Vector) Deserialize(reader readerpkg.IReader, path *pathpkg.Path) Vector {
+	if path == nil {
+		path = pathpkg.Root
+	}
+
+	var items []interface{}
+	tag := uint32(0)
+
+	for !reader.EndOfStream() {
+		tag = tag + 1
+		items = append(items, readerpkg.ReadValue(reader, path, tag))
+	}
+
+	return NewVector(path, items)
+}
+
+func (v *Vector) GetWireType(tag uint32) *codec.WireType {
+	return nil
+}
+
+
+func (v *Vector) GetNested(tag uint32) *statepkg.IState {
+	return nil
+}
+
+func (v *Vector) ReplaceAt(tag uint32, wireType codec.WireType, reader readerpkg.IReader, shouldNotify bool) { if int(tag) >= v.count { panic("index out of range") }
+	panic("not supported")
+}
+
+func (v *Vector) ReplayListPush(reader readerpkg.IReader) {
+	panic("not supported")
+}
+
+func (v *Vector) ReplayListPop() {
+	panic("not supported")
+}
+
+func (v *Vector) ReplayMapRemove(key uint32) {
+	panic("not supported")
+}
