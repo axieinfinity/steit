7eacf20c58f67e99b6524859bb4b0c39b89cb187
Truong Minh Nhat Quang <nhatquangsin@gmail.com>
2022-02-25 16:40:43 +0700

    Add byte reader (#4)
    * Add byte reader
    
    * Update byte reader

--- steit-go/pkg/reader/byte_reader.go
+++ steit-go/pkg/reader/byte_reader.go
@@ -1,5 +1,50 @@
 package reader
 
-func NewByteReader([]byte) IReader {
-	return nil
+var _ IReader = (*ByteReader)(nil)
+
+type ByteReader struct {
+	bytes []byte
+	offset int
 }
+
+func NewByteReader(bytes []byte) IReader {
+	return &ByteReader{bytes: bytes, offset: 0}
+}
+
+func (b *ByteReader) Remaining() int {
+	return len(b.bytes) - b.offset
+}
+
+func (b *ByteReader) ReadUint8() byte {
+	if b.Remaining() <= 0 {
+		panic("end of stream")
+	}
+
+	n := b.bytes[b.offset]
+	b.offset = b.offset + 1
+
+	return n
+}
+
+func (b *ByteReader) Read(count int) []byte {
+	if b.Remaining() <= 0 {
+		panic("end of stream")
+	}
+
+	var bytes []byte
+
+	for i := 0; i < count; i++ {
+		bytes[i] = b.bytes[b.offset]
+		b.offset = b.offset + 1
+	}
+
+	return bytes
+}
+
+func (b *ByteReader) Skip(count int) {
+	if b.Remaining() < count {
+		panic("end of stream")
+	}
+	b.offset += count
+}
+
--- steit-go/pkg/reader/ireader.go
+++ steit-go/pkg/reader/ireader.go
@@ -3,9 +3,8 @@ package reader
 import "github.com/axieinfinity/steit-go/pkg/codec"
 
 type IReader interface {
-	ReadUint32() uint32
-	EndOfStream() bool
-	ReadKey() (uint32, codec.WireType)
-	GetNested() IReader
-	SkipField(int)
+	Remaining() int
+	ReadUint8() byte
+	Read(int) []byte
+	Skip(int)
 }
